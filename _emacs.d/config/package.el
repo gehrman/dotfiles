4b4d6df5ae401f06336120cd231920862ad2db35
Geoff Ehrman <gehrman@gmail.com>
2015-11-17 19:47:13 -0500

    Spin off package installation to its own file.

---
+++ _emacs.d/config/package.el
@@ -0,0 +1,49 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Package: the package manager. ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; Setup package management.
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+(defun ensure-package-installed (&rest packages)
+  "Ensure every package listed is installed. If a packagge is not installed, try to install it.
+   Returns a list of installed packages, or nil if every package is skipped."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+         (package-install package)
+         package)))
+   packages))
+
+; Make sure we have package data.
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+; Activate installed packages.
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+; Packages to use.
+(ensure-package-installed 'evil
+                          'evil-leader
+                          'magit
+                          'anzu
+                          'linum-relative
+                          'powerline
+                          'powerline-evil
+                          'paredit
+                          'helm)
+; Themes. In a different call, b/c they went rogue.
+(ensure-package-installed 'ample-theme
+                          'zenburn-theme
+                          'warm-night-theme
+                          'solarized-theme
+                          'monokai-theme
+                          'darcula-theme
+                          )
--- _emacs.d/init.el
+++ _emacs.d/init.el
@@ -1,65 +1,13 @@
-; Setup path for custom config files.
+; Setup path for custom config files, then load those configs.
 (add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
-(setq mytest (expand-file-name "config" user-emacs-directory))
-;(require 'includetest)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Package: the package manager. ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Setup package management.
-(require 'package)
-
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-(defun ensure-package-installed (&rest packages)
-  "Ensure every package listed is installed. If a package is not installed, try to install it.
-   Returns a list of installed packages, or nil if every package is skipped."
-  (mapcar
-   (lambda (package)
-     (if (package-installed-p package)
-         nil
-       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
-         (package-install package)
-         package)))
-   packages))
-
-; Make sure we have package data.
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
-
-; Activate installed packages.
-(setq package-enable-at-startup nil)
-(package-initialize)
-
-; Packages to use.
-(ensure-package-installed 'evil
-                          'evil-leader
-                          'magit
-                          'anzu
-                          'linum-relative
-                          'powerline
-                          'powerline-evil
-                          'paredit
-                          'helm)
-; Themes. In a different call, b/c they went rogue.
-(ensure-package-installed 'ample-theme
-                          'zenburn-theme
-                          'warm-night-theme
-                          'solarized-theme
-                          'monokai-theme
-                          'darcula-theme
-                          )
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Powerline, without the pain of the vim install. ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'powerline)
 (require 'powerline-evil)
 (powerline-default-theme)
-
+(require 'package-config)
+(require 'powerline-config)
 
 ;;;;;;;;;;;;;;;
 ; Magit Stuff ;
