6f99f950df57edf40fef4e82536389b25414fdf9
Geoff Ehrman <gehrman@gmail.com>
2015-12-03 21:34:28 -0500

    Elisp isn't common lisp.
    So using remove-duplicates is only possible if you load up the common
    lisp package. So let's use that.

--- _emacs.d/config/config-codeblocks.el
+++ _emacs.d/config/config-codeblocks.el
@@ -1,4 +1,5 @@
 ;; Set languages available for execution in code blocks.
+(require 'cl) ; We need the common lisp package for 'remove-duplicates.
 (org-babel-do-load-languages
   'org-babel-load-languages
   (remove-duplicates (append org-babel-load-languages
