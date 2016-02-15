db54d10edeb0b5efc1ab72422c9f923590fd4570
Geoff Ehrman <gehrman@wayfair.com>
2016-02-15 12:59:48 -0500

    Starting point for an install script.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -2,3 +2,26 @@
 # changing _ -> ., generating a diff, saving the diff to an install log, and
 # then using patch to apply the diff. This should allow for uninstallation of
 # problematic revisions. (I think.) Investigate.
+
+pushd `dirname $0` > /dev/null
+cd common
+BASE_DIR=`pwd`
+
+for base_dir in `find . -mindepth 1 -type d`; do
+    target_dir=~/.${dir:3}
+    if [ ! -e $target_dir ]; then
+        echo Creating $target_dir directory.
+        mkdir -p $target_dir
+        echo
+    fi
+done
+
+for file in `find . -mindepth 1 -type f`; do
+    # _file --> .file
+    echo Linking ${file:2} to "~/.${file:3}".
+    #pwd
+    ln -s ${BASE_DIR}/${file:2} ~/.${file:3}
+    echo
+done
+
+popd > /dev/null
