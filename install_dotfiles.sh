0dcd8dc2ff3418e4ba434bb0bfe0b17dec031d7e
Geoff Ehrman <gehrman@wayfair.com>
2016-02-15 16:56:37 -0500

    More dotfig installer work.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,3 +1,5 @@
+#! /usr/bin/env bash
+
 # The strategy here should probably to iterate through the file list,
 # changing _ -> ., generating a diff, saving the diff to an install log, and
 # then using patch to apply the diff. This should allow for uninstallation of
@@ -6,7 +8,6 @@
 pushd `dirname $0` > /dev/null
 cd common
 BASE_DIR=`pwd`
-
 for base_dir in `find . -mindepth 1 -type d`; do
     target_dir=~/.${dir:3}
     if [ ! -e $target_dir ]; then
@@ -15,13 +16,26 @@ for base_dir in `find . -mindepth 1 -type d`; do
         echo
     fi
 done
-
 for file in `find . -mindepth 1 -type f`; do
     # _file --> .file
     echo Linking ${file:2} to "~/.${file:3}".
-    #pwd
     ln -s ${BASE_DIR}/${file:2} ~/.${file:3}
     echo
 done
+popd > /dev/null
 
+pushd `dirname $0` > /dev/null
+HOST=`hostname -s`
+if [[ "${HOST}" =~ ^[bo|dev].* ]] ; then
+    cd site/wayfair
+    BASE_DIR=`pwd`
+    echo Linking wayfair-specific config.
+
+    for file in `find . -mindepth 1 -type f`; do
+        echo Linking ${file:2} to "~/.${file:3}".
+        ln -s ${BASE_DIR}/${file:2} ~/.${file:3}
+    done
+else
+    echo not wayfair
+fi
 popd > /dev/null
