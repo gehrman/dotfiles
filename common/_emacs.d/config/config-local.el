b1d1ed2c2c95279ce2ec74c4c909934da1054a66
Geoff Ehrman <gehrman@wayfair.com>
2016-06-22 10:17:45 -0400

    Work around OS X fullscreen crash

--- common/_emacs.d/config/config-local.el
+++ common/_emacs.d/config/config-local.el
@@ -2,12 +2,19 @@
 
 ;;; Commentary:
 ;; Rather spartan right now, but this will be necessary moving forward.
+;;
+;; For now, this will stay, but be a dispatch for site & os config files.
+;; In particular, it seems like there's going to be a bunch of OS X stuff
+;; that's better off on it's own.
 
 ;;; Code:
 ;; OS aware base directory.
 (if (eq system-type 'windows-nt)
     (setq tramp-default-method "plink")
   (setq tramp-default-method "ssh"))
+
+(if (eq system-type 'darwin)
+    (require 'config-osx))
 
 (provide 'config-local)
 ;;; config-local.el ends here
---
+++ common/_emacs.d/config/config-osx.el
@@ -0,0 +1,25 @@
+;;; config-osx --- OS X specific customizations and settings.
+
+;;; Commentary:
+;; The most pressing fix in here is working around the rather pernicious bug
+;; where quitting (at the least via a :q, which is where I notice it most, but
+;; possibly via other means too) a maximize Emacs gui connected to an Emacs
+;; daemon brings down the daemon and all other clients, not just the gui.
+;; ...Yeah, not a good time.
+;; The bug seems to be specific to Emacs 24, but for now, I don't want to
+;; switch to dev-emacs, wrapping the problematic quit functions is the better
+;; option.
+
+;;; Code:
+(defun demaximize-frame ()
+  "Unmaximize the current frame if maximized, otherwise do nothing."
+  (if (equal (assoc 'fullscreen (frame-parameters))
+             (cons 'fullscreen 'fullboth))
+      (toggle-frame-fullscreen)))
+(defun before-evil-quit (orig-fun &rest args)
+  "Wrap ORIG-FUN and ARGS to work around OS X / Emacs 24 fullscreen quit crash."
+  (demaximize-frame))
+(advice-add 'evil-quit :before #'before-evil-quit)
+
+(provide 'config-osx)
+;;; config-osx.el ends here
