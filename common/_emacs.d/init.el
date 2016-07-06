2bd63d0034f7ced1c20018161c8f63069b37ba3f
Geoff Ehrman <gehrman@wayfair.com>
2016-07-06 14:10:50 -0400

    Tweak background

--- common/_emacs.d/init.el
+++ common/_emacs.d/init.el
@@ -133,7 +133,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "#111111" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Menlo")))))
 
 (provide 'init)
 ;;; init.el ends here
