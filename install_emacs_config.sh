13046e0d9c352965b85f3dd8f85ee83005955f75
Geoff Ehrman <gehrman@wayfair.com>
2016-07-26 17:52:30 -0400

    Tweak emacs install.
    So hard-linking causes problems when git checkout overrides files. Since the
    primary motivation to use hardlinks has been addressed elsewhere, move
    emacs to symlinks. (Other dotfiles always were softlinks.)
    
    See also http://nullprogram.com/blog/2012/06/23/ for other advantages to
    symlinking over hardlinking.

--- install_emacs_config.sh
+++ install_emacs_config.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-[[ -f ~/.emacs.d/init.el ]] && rm -r ~/.emacs.d/init.el
-ln common/_emacs.d/init.el ~/.emacs.d/
 
-[[ -e ~/.emacs.d/config ]] && rm -rf ~/.emacs.d/config && mkdir ~/.emacs.d/config
+# This is somewhat more manual than the current dotfile install process, but
+# we're not going to worry about that for now, since this config has considerably
+# more structure - we only need to create the appropriate directories if they
+# don't yet exist (just .emacs.d and config) and link init.el and the config
+# files.
+
+mkdir -p ~/.emacs.d/config
+ln -s common/_emacs.d/init.el ~/.emacs.d/
+
 for f in `ls common/_emacs.d/config/config-*.el`; do
-    ln $f ~/.emacs.d/config/
+    ln -s $f ~/.emacs.d/config/
 done
