fce1156457540a398ef611c3febe9326f1b76c5b
Geoff Ehrman <gehrman@wayfair.com>
2016-11-01 17:40:07 -0400

    Fix default font problem

--- common/_emacs.d/init.el
+++ common/_emacs.d/init.el
@@ -147,7 +147,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#111111" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Menlo")))))
+ '(default ((t (:inherit nil :stipple nil :background "#111111" :foreground "#bdbdb3" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :width normal)))))
+
+(put 'narrow-to-region 'disabled nil)
 
 (provide 'init)
 ;;; init.el ends here
