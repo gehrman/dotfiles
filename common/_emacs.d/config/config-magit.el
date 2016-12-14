7dd83c9e65d4104f5ceccbaa8fc777e76ee8c7ab
Geoff Ehrman <gehrman@wayfair.com>
2016-12-14 11:28:59 -0500

    Start magit full-frame... so very, very nice

--- common/_emacs.d/config/config-magit.el
+++ common/_emacs.d/config/config-magit.el
@@ -7,6 +7,9 @@
 ;; Shut magit up.
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+;; Make magit start full-frame, but otherwise behave the same.
+(setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+
 ;; Bind C-k to set-mark-command. In magit, I can't seem to reliably stage blocks
 ;; by going into visual mode, C-SPC is launchers, and C-@ is crazy awkward. So,
 ;; I need another binding. C-k is a pneumonic for marK.
