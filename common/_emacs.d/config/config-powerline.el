334d551cc3928d1f89b4f3fd2abc384cb88fea1f
Geoff Ehrman <gehrman@wayfair.com>
2019-01-22 17:59:13 -0500

    Migrate mode-line to telephone from powerline

--- common/_emacs.d/config/config-powerline.el
+++ common/_emacs.d/config/config-powerline.el
@@ -7,18 +7,32 @@
 
 ;;; Code:
 (ensure-package-installed
- 'powerline
- 'powerline-evil
+ 'telephone-line
  )
 
-(require 'powerline)
-(require 'powerline-evil)
-;(powerline-default-theme)
-;(powerline-vim-theme)
-;(powerline-evil-vim-theme)
-;(powerline-evil-vim-color-theme)
-(powerline-evil-center-color-theme)
-(display-time)
+(setq telephone-line-evil-use-short-tag t)
+(telephone-line-defsegment* my/position-segment ()
+  "Return string giving cursor position."
+  (telephone-line-raw "L%l C%c"))
+(telephone-line-defsegment* my/buffer-name-segment ()
+  "Return a buffer-name string, prefixed with ! if modified."
+  (telephone-line-raw
+   (if (buffer-modified-p) (concat "!" (buffer-name)) (buffer-name))))
+
+(setq telephone-line-lhs
+      '((evil my/position-segment)
+        (accent telephone-line-major-mode-segment)
+        (nil telephone-line-projectile-segment)))
+
+(setq telephone-line-center-lhs
+      '((evil my/buffer-name-segment)))
+
+(setq telephone-line-rhs
+      '((nil telephone-line-vc-segment)
+        (accent telephone-line-flycheck-segment)
+        (evil telephone-line-evil-tag-segment)))
+
+(telephone-line-mode 1)
 
 (provide 'config-powerline)
 ;;; config-powerline ends here
--- common/_emacs.d/init.el
+++ common/_emacs.d/init.el
@@ -190,7 +190,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(telephone-line-evil-normal ((t (:inherit telephone-line-evil :background "blue")))))
 
 (put 'narrow-to-region 'disabled nil)
 
