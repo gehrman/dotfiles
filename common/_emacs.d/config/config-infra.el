ab798229cadfbd372776d9f0de0265e40257be63
Geoff Ehrman <gehrman@cmtelematics.com>
2021-10-22 09:50:19 -0400

    Terraform config

--- common/_emacs.d/config/config-infra.el
+++ common/_emacs.d/config/config-infra.el
@@ -1,5 +1,7 @@
 ;;; Package -- Summary
-;;; Commentary: Infrastructure settings for things like puppet, terraform, ansible, etc
+
+;;; Commentary:
+;;; Infrastructure settings for things like puppet, terraform, ansible, etc
 
 ;;; Code:
 (require 'package-tools)
@@ -8,6 +10,13 @@
  'company-terraform
  'terraform-doc
  'terraform-mode)
+
+(defun maybe-terraform-format ()
+  "Run terraform-format-buffer if we're in the correct 'major-mode'."
+  (interactive)
+  (when (equal major-mode 'terraform-mode)
+    (terraform-format-buffer)))
+(add-hook 'before-save-hook 'maybe-terraform-format)
 
 (provide 'config-infra)
 ;;; config-infra ends here
