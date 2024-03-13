d4a4e20ad32e02b51146ae641e115a2728b09bae
Geoff Ehrman <gehrman@cmtelematics.com>
2024-03-12 21:19:35 -0400

    Add helm config

---
+++ common/_emacs.d/package-config/gbe-helm.el
@@ -0,0 +1,14 @@
+;;; gbe-helm --- Configuration for Helm completions
+
+;;; Commentary:
+;;; Helm is complicated, and I don't like it, but for some reason I switched to
+;;; it over IDO, and now I'm used to completions in a separate window even though
+;;; it doesn't work that well
+;;; See:
+;;;   * https://github.com/thierryvolpiatto/emacs-config/blob/main/init-helm.el
+;;;   * https://github.com/emacs-helm/helm/wiki#configure
+
+;;; Code:
+
+(provide 'gbe-helm)
+;;; gbe-helm.el ends here
