4743d21722d964d3f7d98c21e9f97486bccd2598
Geoff Ehrman <gehrman@gmail.com>
2025-04-19 10:22:27 -0400

    Add ansible and vterm modes

---
+++ common/_emacs.d/package-config/gbe-devops.el
@@ -0,0 +1,11 @@
+;;; gbe-devops -- Tools to do infrastructure work
+
+;;; Commentary:
+;; Infrastructure settings for things like puppet, terraform, ansible, etc
+
+;;; Code:
+(use-package ansible
+  :straight t)
+
+(provide 'gbe-devops)
+;;; gbe-devops.el ends here
